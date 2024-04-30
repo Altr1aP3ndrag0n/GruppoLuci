e3ef61885dcafe9b53f0149500b21f43b31f2466
Altr1aP3ndrag0n <126151498+Altr1aP3ndrag0n@users.noreply.github.com>
2024-04-30 15:23:13 +0200

    Update newReport.php

--- newReport.php
+++ newReport.php
@@ -5,11 +5,9 @@ $db = $conn;
 $s = "ciao";
 $file = fopen('csv.csv', 'r');
 $data = fgetcsv($file, 1000, ";"); 
-$all_record_array = [];
 $pesi = [];
 $PesoUtilizzatos = [];
 while (($data = fgetcsv($file, 1024,";"))!== FALSE) {
-    $all_record_array[] = $data;
     $Codice = $data[0];
     $weight = (int)$data[1]; 
     if (!isset($pesi[$Codice])) {
@@ -18,19 +16,24 @@ while (($data = fgetcsv($file, 1024,";"))!== FALSE) {
     }
     $pesi[$Codice][] = $weight;
 }
+
+
+
 fclose($file);
 
 foreach ($pesi as $Codice => $ListaPesi) {
     $PesoPulito = array_sum($ListaPesi) / count($ListaPesi);
     $PesoUtilizzato = $PesoUtilizzatos[$Codice];
     $material_collected = $PesoPulito - (int)$PesoUtilizzato;
-
-    $stmt = $db->prepare("INSERT INTO Filtri (Codice, PesoPulito, PesoUtilizzato) VALUES (:Codice, :PesoPulito, :PesoUtilizzato)");
-    $stmt->bindParam(':PesoUtilizzato', $PesoUtilizzato);
-    $stmt->bindParam(':Codice', $Codice);
-    $stmt->bindParam(':PesoPulito', $PesoPulito);
-    $stmt->execute();
- 
-    echo "Codice: $Codice, PesoPulito: $PesoPulito, Peso utilizzato: $PesoUtilizzaton";
+    
+    
+    if (!is_int($Codice)) {
+        continue;
     }
+
+    $stmt = $db->prepare("INSERT INTO Filtri (Codice, PesoPulito, PesoUtilizzato) VALUES (?,?,?)");
+    $stmt->bind_param("iff", $Codice, $PesoPulito, $PesoUtilizzato);
+    $stmt->execute();    
+}
+//echo "Codice: $Codice, Average weight: $PesoPulito, Material collected: $material_collected\n"; se volete vedere cosa stampa
 ?>
