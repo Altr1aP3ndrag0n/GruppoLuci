ffd63b5d9d9308ff584adb027d8c6e3e30fe7855
Altr1aP3ndrag0n <126151498+Altr1aP3ndrag0n@users.noreply.github.com>
2024-04-30 18:42:38 +0200

    Update newReport.php

--- newReport.php
+++ newReport.php
@@ -7,17 +7,24 @@ $file = fopen('csv.csv', 'r');
 $data = fgetcsv($file, 1000, ";"); 
 $pesi = [];
 $PesoUtilizzatos = [];
+
+$Codice = $data[1];
 while (($data = fgetcsv($file, 1024,";"))!== FALSE) {
-    $Codice = $data[0];
-    $weight = (int)$data[1]; 
+    if ($data[1]) {
+        echo $data[1]. 'parte iniziale'. "\n";
+        if (strtolower(substr($Codice, 0, 1)) === 'c') {
+            echo 'parte secondaria'. "\n";
+        }
+        $Codice = $data[1];
+    }
+    $weight = (int)$data[1];
     if (!isset($pesi[$Codice])) {
         $pesi[$Codice] = [];
         $PesoUtilizzatos[$Codice] = $weight;
     }
     $pesi[$Codice][] = $weight;
+    var_dump($data[1]);
 }
-
-
 
 fclose($file);
 
@@ -25,15 +32,21 @@ foreach ($pesi as $Codice => $ListaPesi) {
     $PesoPulito = array_sum($ListaPesi) / count($ListaPesi);
     $PesoUtilizzato = $PesoUtilizzatos[$Codice];
     $material_collected = $PesoPulito - (int)$PesoUtilizzato;
-    
-    
-    if (!is_int($Codice)) {
-        continue;
-    }
 
     $stmt = $db->prepare("INSERT INTO Filtri (Codice, PesoPulito, PesoUtilizzato) VALUES (?,?,?)");
-    $stmt->bind_param("iff", $Codice, $PesoPulito, $PesoUtilizzato);
+    $stmt->bind_param("sii", $Codice, $PesoPulito, $PesoUtilizzato);
     $stmt->execute();    
 }
-//echo "Codice: $Codice, Average weight: $PesoPulito, Material collected: $material_collected\n"; se volete vedere cosa stampa
+
+$sql = "SELECT * FROM Filtri";
+$query = mysqli_prepare($db, $sql);
+mysqli_stmt_execute($query);
+$ris = mysqli_stmt_get_result($query);
+
+if (mysqli_num_rows($ris) > 0) {
+    while ($row = mysqli_fetch_assoc($ris)) {
+    }
+} else {
+    echo "No data found in the Filtri table.\n";
+}
 ?>
