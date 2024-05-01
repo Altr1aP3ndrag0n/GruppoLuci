c459c84e9ec9d2bdfa1ec6bda9b9ddcff074ff99
Altr1aP3ndrag0n <126151498+Altr1aP3ndrag0n@users.noreply.github.com>
2024-05-01 09:19:24 +0200

    Update newReport.php

--- newReport.php
+++ newReport.php
@@ -1,52 +1,44 @@
 <?php
+define('ABS_PATH', $_SERVER['DOCUMENT_ROOT'] . '/');
 include_once 'db_connect.php';
 $db = $conn;
 
 $s = "ciao";
-$file = fopen('csv.csv', 'r');
-$data = fgetcsv($file, 1000, ";"); 
-$pesi = [];
-$PesoUtilizzatos = [];
+$namefile = "csv.csv";
+$file = fopen($namefile, 'r');
+fgetcsv($file, 1000, ";"); // Skip the header row
 
-$Codice = $data[1];
-while (($data = fgetcsv($file, 1024,";"))!== FALSE) {
-    if ($data[1]) {
-        echo $data[1]. 'parte iniziale'. "\n";
-        if (strtolower(substr($Codice, 0, 1)) === 'c') {
-            echo 'parte secondaria'. "\n";
-        }
-        $Codice = $data[1];
-    }
-    $weight = (int)$data[1];
-    if (!isset($pesi[$Codice])) {
-        $pesi[$Codice] = [];
-        $PesoUtilizzatos[$Codice] = $weight;
-    }
-    $pesi[$Codice][] = $weight;
-    var_dump($data[1]);
+$pesi = [];
+$Pesoutilizzato = [];
+$codice = [];
+
+while (($data = fgetcsv($file, 1024, ";"))!== FALSE) {
+    $codice[] = $data[1];
+    $pesi[] = $data[3];
+    $Pesoutilizzato[] = $data[3];
 }
 
 fclose($file);
-
-foreach ($pesi as $Codice => $ListaPesi) {
-    $PesoPulito = array_sum($ListaPesi) / count($ListaPesi);
-    $PesoUtilizzato = $PesoUtilizzatos[$Codice];
-    $material_collected = $PesoPulito - (int)$PesoUtilizzato;
-
+foreach ($pesi as $i => $data) {
+    $PesoPulito = $Pesoutilizzato[$i]/count(array_keys($pesi));
     $stmt = $db->prepare("INSERT INTO Filtri (Codice, PesoPulito, PesoUtilizzato) VALUES (?,?,?)");
-    $stmt->bind_param("sii", $Codice, $PesoPulito, $PesoUtilizzato);
-    $stmt->execute();    
+    $stmt->bind_param("sii", $codice[$i], $PesoPulito, $Pesoutilizzato[$i]);
+    $stmt->execute();
 }
 
-$sql = "SELECT * FROM Filtri";
-$query = mysqli_prepare($db, $sql);
-mysqli_stmt_execute($query);
-$ris = mysqli_stmt_get_result($query);
 
-if (mysqli_num_rows($ris) > 0) {
-    while ($row = mysqli_fetch_assoc($ris)) {
+$file =  ABS_PATH . 'csv.csv';
+$dest_path = ABS_PATH . 'archivio';
+
+if(file_exists($dest_path)) {
+	if(rename($file, $dest_path . '/csv.csv')) {
+        echo "File successfully moved";
     }
-} else {
-    echo "No data found in the Filtri table.\n";
+    else {
+        echo "Error moving file";
+    }
 }
+
+
+
 ?>
